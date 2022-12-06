442759ed7cbe0ae32cc9c2ef284ff16578ade97d
Trevor French <trevorf96@gmail.com>
2022-12-05 17:43:19 -0700

    cex_io functions

--- R/cex_io.R
+++ R/cex_io.R
@@ -3,6 +3,7 @@
 #-------------------------------------------------------------------------------
 # - Don't know what the ticker symbol by market endpoint is doing. did not create function
 # - Same for last markets
+# - getting 500s on cex_io_converter
 
 #-------------------------------------------------------------------------------
 #-----------------------------------CEX.IO TICKER-------------------------------
@@ -66,6 +67,35 @@ cex_io_currency_limits <- function() {
 
 cex_io_last_price <- function(symbol_1, symbol_2) {
   res = httr::GET(paste('https://cex.io/api/last_price/', toupper(symbol_1), '/', toupper(symbol_2), sep = ''))
+  data = jsonlite::fromJSON(rawToChar(res$content))
+  return(data)
+}
+
+#-------------------------------------------------------------------------------
+#----------------------------------CEX.IO CONVERTER-----------------------------
+#-------------------------------------------------------------------------------
+#' cex_io_converter
+#'
+#' @param symbol_1 the first currency in your pair
+#' @param symbol_2 the second currency in your pair
+#'
+#' @return returns a list with the last price of your specified currency pair.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' symbol_1 <- 'btc'
+#' symbol_2 <- 'usd'
+#' last_price <- cex_io_converter(symbol_1, symbol_2)}
+
+amount <- '1'
+
+cex_io_converter <- function(symbol_1, symbol_2) {
+  res = httr::POST(paste('https://cex.io/api/convert/', toupper(symbol_1), '/', toupper(symbol_2), sep = '')
+                   , body = list('amnt' = amount)
+                   , httr::add_headers('Content-Type' = 'application/json'
+                                      , 'accept' = '*/*'
+                   ))
   data = jsonlite::fromJSON(rawToChar(res$content))
   return(data)
 }
