ac7c8fd40c229572edb27ce2dcb140eb74eaa1f3
Trevor French <trevorf96@gmail.com>
2022-12-27 14:31:05 -0700

    magic_eden functions

--- R/magic_eden.R
+++ R/magic_eden.R
@@ -1,7 +1,7 @@
 #-------------------------------------------------------------------------------
 #------------------------------------ACTION LIST--------------------------------
 #-------------------------------------------------------------------------------
-# - Add offset and limit parameters
+# -
 
 #-------------------------------------------------------------------------------
 #------------------------------MAGIC EDEN TRANSACTIONS--------------------------
@@ -9,8 +9,13 @@
 #' magic_eden_transactions
 #'
 #' @param wallet the address of the wallet you are trying to query
+#' @param offset optionally provide a numeric value to specify number of
+#' transactions to skip.
+#' @param limit optionally provide a numeric limit to specify maximum number of
+#' results.
 #'
-#' @return returns a dataframe containing all transactions for the specified wallet.
+#' @return returns a dataframe containing all transactions for the specified
+#' wallet.
 #' @export
 #'
 #' @examples
@@ -18,8 +23,99 @@
 #' wallet <- "72tXz6jhGVPFE8ZfAQocJPJU3HgxsdrRqKZoUdWUhs7o"
 #' transactions <- magic_eden_transactions(wallet)}
 
-magic_eden_transactions <- function(wallet) {
-  res = httr::GET(paste('http://api-mainnet.magiceden.dev/v2/wallets/', wallet, '/activities', sep = ''))
+magic_eden_transactions <- function(wallet, offset = NULL, limit = NULL) {
+  url <- paste('http://api-mainnet.magiceden.dev/v2/wallets/'
+               , wallet
+               , '/activities'
+               , sep = '')
+  query <- list(offset = offset, limit = limit)
+  res = httr::GET(url, query = query)
+  data = jsonlite::fromJSON(rawToChar(res$content))
+  return(data)
+}
+
+#-------------------------------------------------------------------------------
+#------------------------------MAGIC EDEN TOKENS OWNED--------------------------
+#-------------------------------------------------------------------------------
+#' magic_eden_tokens_owned
+#'
+#' @param wallet the address of the wallet you are trying to query
+#' @param offset optionally provide a numeric value to specify number of
+#' results to skip.
+#' @param limit optionally provide a numeric limit to specify maximum number of
+#' results.
+#' @param list_status either "listed", "unlisted" or "both". The default is
+#' "both".
+#'
+#' @return returns a dataframe containing all tokens owned by specified wallet.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' wallet <- "72tXz6jhGVPFE8ZfAQocJPJU3HgxsdrRqKZoUdWUhs7o"
+#' tokens <- magic_eden_tokens_owned(wallet)}
+
+magic_eden_tokens_owned <- function(wallet
+                                    , offset = NULL
+                                    , limit = NULL
+                                    , list_status = NULL) {
+  url <- paste('http://api-mainnet.magiceden.dev/v2/wallets/'
+               , wallet
+               , '/tokens'
+               , sep = '')
+  query <- list(offset = offset, limit = limit, listStatus = list_status)
+  res = httr::GET(url, query = query)
+  data = jsonlite::fromJSON(rawToChar(res$content))
+  return(data)
+}
+
+#-------------------------------------------------------------------------------
+#-----------------------------MAGIC EDEN TOKEN METADATA-------------------------
+#-------------------------------------------------------------------------------
+#' magic_eden_token_metadata
+#'
+#' @param mint_address the mint address of the token you wish to query
+#'
+#' @return returns a list containing the token metadata for the specified mint
+#' address.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' mint_address <- "Hd6sxFEEQQA5aURaWaDesi23AkM19bBkKave1hyWvnfS"
+#' metadata <- magic_eden_token_metadata(mint_address)}
+
+magic_eden_token_metadata <- function(mint_address) {
+  url <- paste('http://api-mainnet.magiceden.dev/v2/tokens/'
+               , mint_address
+               , sep = '')
+  res = httr::GET(url)
+  data = jsonlite::fromJSON(rawToChar(res$content))
+  return(data)
+}
+
+#-------------------------------------------------------------------------------
+#-----------------------------MAGIC EDEN TOKEN LISTINGS-------------------------
+#-------------------------------------------------------------------------------
+#' magic_eden_token_listings
+#'
+#' @param mint_address the mint address of the token you wish to query
+#'
+#' @return returns a dataframe containing the token listings for the specified
+#' mint address.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' mint_address <- "Hd6sxFEEQQA5aURaWaDesi23AkM19bBkKave1hyWvnfS"
+#' listings <- magic_eden_token_listings(mint_address)}
+
+magic_eden_token_listings <- function(mint_address) {
+  url <- paste('http://api-mainnet.magiceden.dev/v2/tokens/'
+               , mint_address
+               , '/listings'
+               , sep = '')
+  res = httr::GET(url)
   data = jsonlite::fromJSON(rawToChar(res$content))
   return(data)
 }
--- docs/magic_eden.md
+++ docs/magic_eden.md
@@ -6,17 +6,66 @@
 
 ## Functions:
 
-magic_eden_transactions
+magic_eden_transactions, magic_eden_tokens_owned, magic_eden_token_metadata, magic_eden_token_listings
 
-### magic_eden_transactions(wallet)
+### magic_eden_transactions(wallet, offset, limit)
 
 -   Returns a dataframe containing all transactions for the specified wallet.
 
 -   wallet: the address of the wallet you are trying to query
+
+-   offset: optionally provide a numeric value to specify number of transactions to skip.
+
+-   limit: optionally provide a numeric limit to specify maximum number of results.
 
 -   The following example retrieves the Magic Eden transactions for the specified wallet address and stores the data in a dataframe named "transactions".
 
 ``` r
 wallet <- "72tXz6jhGVPFE8ZfAQocJPJU3HgxsdrRqKZoUdWUhs7o"
 transactions <- magic_eden_transactions(wallet)
+```
+
+### magic_eden_tokens_owned(wallet, offset, limit, list_status)
+
+-   Returns a dataframe containing all tokens owned by specified wallet.
+
+-   wallet: the address of the wallet you are trying to query
+
+-   offset: optionally provide a numeric value to specify number of results to skip.
+
+-   limit: optionally provide a numeric limit to specify maximum number of results.
+
+-   list_status: either "listed", "unlisted" or "both". The default is "both".
+
+-   The following example retrieves the tokens owned by the specified wallet and stores the resulting data in a dataframe named "tokens".
+
+``` r
+wallet <- "72tXz6jhGVPFE8ZfAQocJPJU3HgxsdrRqKZoUdWUhs7o"
+tokens <- magic_eden_tokens_owned(wallet)
+```
+
+### magic_eden_token_metadata(mint_address)
+
+-   Returns a list containing the token metadata for the specified mint address.
+
+-   mint_address: the mint address of the token you wish to query
+
+-   The following example retrieves the token metadata for the specified mint address and stores the resulting data in a list named "metadata".
+
+``` r
+mint_address <- "Hd6sxFEEQQA5aURaWaDesi23AkM19bBkKave1hyWvnfS"
+metadata <- magic_eden_token_metadata(mint_address)
+```
+
+### magic_eden_token_listings(mint_address)
+
+-   Returns a dataframe containing the token listings for the specified mint address.
+
+-   mint_address: the mint address of the token you wish to query
+
+-   The following example retrieves the token listings for the specified mint address and stores the resulting data in a dataframe named "listings".
+
+``` r
+mint_address <- "Hd6sxFEEQQA5aURaWaDesi23AkM19bBkKave1hyWvnfS"
+listings <- magic_eden_token_listings(mint_address)
 ```
