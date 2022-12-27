369059b2cc51bc509908c989763c92b2f64b6960
Trevor French <trevorf96@gmail.com>
2022-12-27 14:03:02 -0700

    nifty_gateway functions

--- R/nifty_gateway.R
+++ R/nifty_gateway.R
@@ -1,8 +1,7 @@
 #-------------------------------------------------------------------------------
 #------------------------------------ACTION LIST--------------------------------
 #-------------------------------------------------------------------------------
-# - Add limit, offset, and contractAddress parameters
-# - Add custom endpoints created on last Nifty Gateway app
+# -
 
 #-------------------------------------------------------------------------------
 #----------------------------NIFTY GATEWAY USER NIFTIES-------------------------
@@ -10,16 +9,65 @@
 #' nifty_gateway_user_nifties
 #'
 #' @param username the username you wish to query
+#' @param limit optionally provide the maximum number of results to return. This
+#' is a numeric parameter.
+#' @param offset optionally specify how many results to skip. This is a numeric
+#' parameter.
+#' @param contract_address optionally filter results by contract address.
 #'
-#' @return returns a dataframe containing information about the nifties owned by the specified user
+#' @return returns a dataframe containing information about the nifties owned by
+#' the specified user
 #' @export
 #'
 #' @examples
 #' \dontrun{
 #' nifties <- nifty_gateway_user_nifties('tommy')}
 
-nifty_gateway_user_nifties <- function(username) {
-  res = httr::GET(paste('https://api.niftygateway.com/v1/users/', username, '/nifties/', sep = ''))
+nifty_gateway_user_nifties <- function(username
+                                       , limit = NULL
+                                       , offset = NULL
+                                       , contract_address = NULL) {
+  url <- paste('https://api.niftygateway.com/v1/users/'
+               , username
+               , '/nifties/'
+               , sep = ''
+               )
+  query <- list(contractAddress = contract_address
+                , limit = limit
+                , offset = offset
+                )
+  res = httr::GET(url
+                  , query = query)
+  data = jsonlite::fromJSON(rawToChar(res$content))
+  return(data$results)
+}
+
+#-------------------------------------------------------------------------------
+#------------------------------NIFTY GATEWAY CREATORS---------------------------
+#-------------------------------------------------------------------------------
+#' nifty_gateway_creators
+#'
+#' @param username the username you wish to query
+#' @param limit optionally provide the maximum number of results to return. This
+#' is a numeric parameter.
+#' @param offset optionally specify how many results to skip. This is a numeric
+#' parameter.
+#'
+#' @return returns a dataframe containing ownership information for all nifties
+#' created by the given creator and currently stored on Nifty Gateway
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' creators <- nifty_gateway_creators('beeple')}
+
+nifty_gateway_creators <- function(username, limit = NULL, offset = NULL) {
+  url <- paste('https://api.niftygateway.com/v1/creators/'
+               , username
+               , '/collectors/'
+               , sep = '')
+  query <- list(limit = limit, offset = offset)
+  res = httr::GET(url, query = query)
   data = jsonlite::fromJSON(rawToChar(res$content))
   return(data$results)
 }
--- docs/nifty_gateway.md
+++ docs/nifty_gateway.md
@@ -6,16 +6,38 @@
 
 ## Functions:
 
-nifty_gateway_user_nifties
+nifty_gateway_user_nifties, nifty_gateway_creators
 
-### nifty_gateway_user_nifties(username)
+### nifty_gateway_user_nifties(username, limit, offset, contract_address)
 
 -   Returns a dataframe containing information about the nifties owned by the specified user
 
 -   username: the username you wish to query
 
+-   limit: optionally provide the maximum number of results to return. This is a numeric parameter.
+
+-   offset: optionally specify how many results to skip. This is a numeric parameter.
+
+-   contract_address: optionally filter results by contract address.
+
 -   The following example retrieves the nifties for the user named "tommy" and stores the resulting data in a dataframe named "nifties".
 
 ``` r
 nifties <- nifty_gateway_user_nifties('tommy')
+```
+
+### nifty_gateway_creators(username, limit, offset)
+
+-   Returns a dataframe containing ownership information for all nifties created by the given creator and currently stored on Nifty Gateway
+
+-   username: the username you wish to query
+
+-   limit: optionally provide the maximum number of results to return. This is a numeric parameter.
+
+-   offset: optionally specify how many results to skip. This is a numeric parameter.
+
+-   The following example retrieves the ownership information for nifties created by Beeple and stores the data in a dataframe named "creators".
+
+``` r
+creators <- nifty_gateway_creators('beeple')
 ```
