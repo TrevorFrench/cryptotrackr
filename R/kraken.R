0e7c808a94e68637071a59785b514501ad782376
Trevor French <trevorf96@gmail.com>
2022-12-27 20:14:46 -0700

    kraken functions

--- R/kraken.R
+++ R/kraken.R
@@ -38,3 +38,74 @@ kraken_server_status <- function() {
   data = jsonlite::fromJSON(rawToChar(res$content))
   return(data$result)
 }
+
+#-------------------------------------------------------------------------------
+#--------------------------------KRAKEN ASSET INFO------------------------------
+#-------------------------------------------------------------------------------
+#' kraken_asset_info
+#'
+#' @param asset optionally provide one or more comma-separated ticker symbols.
+#' @param aclass optionally provide asset categories to filter by.
+#'
+#' @return returns a list containing asset information
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' all_asset_info <- kraken_asset_info()
+#' eth_btc_info <- kraken_asset_info("ETH,BTC")
+#' currency_info <- kraken_asset_info(aclass = "currency")}
+
+kraken_asset_info <- function(asset = NULL, aclass = NULL) {
+  query <- list(asset = asset, aclass = aclass)
+  res = httr::GET('https://api.kraken.com/0/public/Assets', query = query)
+  data = jsonlite::fromJSON(rawToChar(res$content))
+  return(data$result)
+}
+
+#-------------------------------------------------------------------------------
+#--------------------------------KRAKEN ASSET PAIRS-----------------------------
+#-------------------------------------------------------------------------------
+#' kraken_asset_pairs
+#'
+#' @param pair optionally provide one or more comma-separated asset pairs to query.
+#' @param info optionally select the information to return. You can choose from:
+#' "info" (all info), "leverage" (leverage info), "fees" (fee schedule), or
+#' "margin" (margin info).
+#'
+#' @return returns a list containing information on Kraken asset pairs.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' asset_pairs <- kraken_asset_pairs()}
+
+kraken_asset_pairs <- function(pair = NULL, info = NULL) {
+  query <- list(pair = pair, info = info)
+  res = httr::GET('https://api.kraken.com/0/public/AssetPairs', query = query)
+  data = jsonlite::fromJSON(rawToChar(res$content))
+  return(data$result)
+}
+
+#-------------------------------------------------------------------------------
+#--------------------------------KRAKEN TICKER INFO-----------------------------
+#-------------------------------------------------------------------------------
+#' kraken_ticker_info
+#'
+#' @param pair optionally provide one or more comma-separated asset pairs.
+#'
+#' @return returns a list containing ticker info for assets on Kraken. Refer to
+#' Kraken for help interpreting response data:
+#' https://docs.kraken.com/rest/#tag/Market-Data/operation/getTickerInformation
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' ticker_info <- kraken_ticker_info("ETHUSD")}
+
+kraken_ticker_info <- function(pair = NULL) {
+  query <- list(pair = pair)
+  res = httr::GET('https://api.kraken.com/0/public/Ticker', query = query)
+  data = jsonlite::fromJSON(rawToChar(res$content))
+  return(data$result)
+}
--- docs/kraken.md
+++ docs/kraken.md
@@ -6,7 +6,7 @@
 
 ## Functions:
 
-kraken_server_time, kraken_server_status
+kraken_server_time, kraken_server_status, kraken_asset_info, kraken_asset_pairs, kraken_ticker_info
 
 ### kraken_server_time()
 
@@ -26,4 +26,46 @@ time <- kraken_server_time()
 
 ``` r
 status <- kraken_server_status()
+```
+
+### kraken_asset_info(asset, aclass)
+
+-   Returns a list containing asset information.
+
+-   asset: optionally provide one or more comma-separated ticker symbols.
+
+-   aclass: optionally provide asset categories to filter by.
+
+-   The following example calls the function three times. The first call accept all defaults and stores the maximum amount of information in a list named "all_asset_info". The second example only pulls information for ETH and BTC and stores the information in a list named "eth_btc_info". The third example only pulls information for assets which are classified as currencies and stores the information in a list named "currency_info".
+
+``` r
+all_asset_info <- kraken_asset_info()
+eth_btc_info <- kraken_asset_info("ETH,BTC")
+currency_info <- kraken_asset_info(aclass = "currency")
+```
+
+### kraken_asset_pairs(pair, info)
+
+-   Returns a list containing information on Kraken asset pairs.
+
+-   pair: optionally provide one or more comma-separated asset pairs to query.
+
+-   info: optionally select the information to return. You can choose from: "info" (all info), "leverage" (leverage info), "fees" (fee schedule), or "margin" (margin info).
+
+-   The following example gets a list containing information on all asset pairs available to trade on Kraken and stores the data in a list named "asset_pairs".
+
+``` r
+asset_pairs <- kraken_asset_pairs()
+```
+
+### kraken_ticker_info(pair)
+
+-   Returns a list containing ticker info for assets on Kraken. Refer to Kraken for help interpreting response data: <https://docs.kraken.com/rest/#tag/Market-Data/operation/getTickerInformation>
+
+-   pair optionally provide one or more comma-separated asset pairs.
+
+-   The following example gets a list containing ticker information for "ETHUSD" and stores the data in a list named "ticker_info".
+
+``` r
+ticker_info <- kraken_ticker_info("ETHUSD")
 ```
