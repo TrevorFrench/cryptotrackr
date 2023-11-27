d4e1f31a0faf2a630b2fcb87c65e9306c269da1a
Trevor French <trevorf96@gmail.com>
2023-11-27 18:19:25 -0500

    removing blockchain.com comment flair

--- R/blockchain_dot_com.R
+++ R/blockchain_dot_com.R
@@ -1,17 +1,9 @@
-#-------------------------------------------------------------------------------
-#------------------------------------ACTION LIST--------------------------------
-#-------------------------------------------------------------------------------
-# - Maybe combine ticker and ticker symbol functions and also rename
-# - 500 errors on authenticated methods
-
-#-------------------------------------------------------------------------------
-#---------------------------BLOCKCHAIN.COM L2 ORDER BOOK------------------------
-#-------------------------------------------------------------------------------
 #' blockchain_dot_com_l2_order_book
 #'
 #' @param symbol the symbol for which to retrieve data
 #'
-#' @return returns a list containing a 'bids' and an 'asks' dataframe along with the specified symbol
+#' @return returns a list containing a 'bids' and an 'asks' dataframe along with
+#' the specified symbol
 #' @export
 #'
 #' @examples
@@ -22,19 +14,18 @@
 #' asks <- l2_order_book$asks}
 
 blockchain_dot_com_l2_order_book <- function(symbol) {
-  res = httr::GET(paste('https://api.blockchain.com/v3/exchange/l2/', toupper(symbol), sep = ''))
+  res = httr::GET(paste('https://api.blockchain.com/v3/exchange/l2/',
+                        toupper(symbol), sep = ''))
   data = jsonlite::fromJSON(rawToChar(res$content))
   return(data)
 }
 
-#-------------------------------------------------------------------------------
-#---------------------------BLOCKCHAIN.COM L3 ORDER BOOK------------------------
-#-------------------------------------------------------------------------------
 #' blockchain_dot_com_l3_order_book
 #'
 #' @param symbol the symbol for which to retrieve data
 #'
-#' @return returns a list containing a 'bids' and an 'asks' dataframe along with the specified symbol
+#' @return returns a list containing a 'bids' and an 'asks' dataframe along with
+#' the specified symbol
 #' @export
 #'
 #' @examples
@@ -45,14 +36,12 @@ blockchain_dot_com_l2_order_book <- function(symbol) {
 #' asks <- l3_order_book$asks}
 
 blockchain_dot_com_l3_order_book <- function(symbol) {
-  res = httr::GET(paste('https://api.blockchain.com/v3/exchange/l3/', toupper(symbol), sep = ''))
+  res = httr::GET(paste('https://api.blockchain.com/v3/exchange/l3/',
+                        toupper(symbol), sep = ''))
   data = jsonlite::fromJSON(rawToChar(res$content))
   return(data)
 }
 
-#-------------------------------------------------------------------------------
-#------------------------------BLOCKCHAIN.COM TICKERS---------------------------
-#-------------------------------------------------------------------------------
 #' blockchain_dot_com_tickers
 #'
 #' @return returns a dataframe with price and volume data for all symbols
@@ -68,9 +57,6 @@ blockchain_dot_com_tickers <- function() {
   return(data)
 }
 
-#-------------------------------------------------------------------------------
-#---------------------------BLOCKCHAIN.COM TICKER SYMBOL------------------------
-#-------------------------------------------------------------------------------
 #' blockchain_dot_com_ticker_symbol
 #'
 #' @param symbol the symbol for which to retrieve data
@@ -84,14 +70,12 @@ blockchain_dot_com_tickers <- function() {
 #' ticker_data <- blockchain_dot_com_ticker_symbol(symbol)}
 
 blockchain_dot_com_ticker_symbol <- function(symbol) {
-  res = httr::GET(paste('https://api.blockchain.com/v3/exchange/tickers/', toupper(symbol), sep = ''))
+  res = httr::GET(paste('https://api.blockchain.com/v3/exchange/tickers/',
+                        toupper(symbol), sep = ''))
   data = jsonlite::fromJSON(rawToChar(res$content))
   return(data)
 }
 
-#-------------------------------------------------------------------------------
-#------------------------------BLOCKCHAIN.COM SYMBOLS---------------------------
-#-------------------------------------------------------------------------------
 #' blockchain_dot_com_symbols
 #'
 #' @return returns a list with various data for all symbols
@@ -107,9 +91,6 @@ blockchain_dot_com_symbols <- function() {
   return(data)
 }
 
-#-------------------------------------------------------------------------------
-#-------------------------------BLOCKCHAIN.COM SYMBOL---------------------------
-#-------------------------------------------------------------------------------
 #' blockchain_dot_com_symbol
 #'
 #' @param symbol the symbol for which to retrieve data
@@ -123,7 +104,8 @@ blockchain_dot_com_symbols <- function() {
 #' symbol_data <- blockchain_dot_com_symbol(symbol)}
 
 blockchain_dot_com_symbol <- function(symbol) {
-  res = httr::GET(paste('https://api.blockchain.com/v3/exchange/symbols/', toupper(symbol), sep = ''))
+  res = httr::GET(paste('https://api.blockchain.com/v3/exchange/symbols/',
+                        toupper(symbol), sep = ''))
   data = jsonlite::fromJSON(rawToChar(res$content))
   return(data)
 }
