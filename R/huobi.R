4ed4cef18009a71b643ed35afe9269450e55f29c
Trevor French <trevorf96@gmail.com>
2023-12-02 12:17:43 -0500

    huobi comment flair

--- R/huobi.R
+++ R/huobi.R
@@ -1,16 +1,10 @@
-#-------------------------------------------------------------------------------
-#------------------------------------ACTION LIST--------------------------------
-#-------------------------------------------------------------------------------
-# -
-
-
-#-------------------------------------------------------------------------------
-#-----------------------------------HUOBI CANDLES-------------------------------
-#-------------------------------------------------------------------------------
 #' huobi_candles
 #'
-#' @param period the period of each candle. The following are acceptable options:	"1min", "5min", "15min", "30min", "60min", "4hour", "1day", "1mon", "1week", "1year"
-#' @param size the number of datapoints to return. This should fall between 1 and 2000.
+#' @param period the period of each candle. The following are acceptable options:
+#' "1min", "5min", "15min", "30min", "60min", "4hour", "1day", "1mon", "1week",
+#' "1year"
+#' @param size the number of datapoints to return. This should fall between 1
+#' and 2000.
 #' @param symbol the trading symbol to query.
 #'
 #' @return returns a dataframe containing Huobi candle data
