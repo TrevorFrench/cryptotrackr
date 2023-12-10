dcd546fa8663f4794046fdc53c5ca6ceea52a951
Trevor French <trevorf96@gmail.com>
2023-12-09 20:47:19 -0500

    gemini unit tests

--- tests/testthat/test-gemini.R
+++ tests/testthat/test-gemini.R
@@ -1,18 +1,11 @@
 library(testthat)
 
-context('Crypto.com Public Calls')
+context('Gemini Public Calls')
 
-test_that('the "crypto_dot_com_instruments" function returns a dataframe', {
-  expect_s3_class(crypto_dot_com_instruments(), 'data.frame')
+test_that('the "gemini_symbols" function returns a character vector', {
+  expect_type(gemini_symbols(), 'character')
 })
 
-
-# gemini_symbols, gemini_price_feed, huobi_candles,
-# kraken_server_time, kraken_server_status,
-# kraken_asset_info, kraken_asset_pairs,
-# kraken_ticker_info, kucoin_time, kucoin_symbols_list,
-# magic_eden_collection_stats,
-# magic_eden_token_listings, magic_eden_token_metadata,
-# magic_eden_tokens_owned, magic_eden_transactions,
-# okcoin_trading_pairs, okcoin_time
-# bittrex source, c source xt
+test_that('the "gemini_price_feed" function returns a dataframe', {
+  expect_s3_class(gemini_price_feed(), 'data.frame')
+})
