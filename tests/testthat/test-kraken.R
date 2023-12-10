53d2aa611f847f4639f7f5ff29b2e5452d8f180d
Trevor French <trevorf96@gmail.com>
2023-12-09 21:43:35 -0500

    working on kraken unit tests

--- tests/testthat/test-kraken.R
+++ tests/testthat/test-kraken.R
@@ -2,11 +2,22 @@ library(testthat)
 
 context('Kraken Public Calls')
 
-test_that('the "huobi_candles" function returns a dataframe', {
-  expect_s3_class(huobi_candles('1day', '200', 'btcusdt'), 'data.frame')
+test_that('the "kraken_server_time" function returns unixtime as an integer and
+          rfc1123 as a character', {
+  expect_type(kraken_server_time()$unixtime, 'integer')
+  expect_type(kraken_server_time()$rfc1123, 'character')
 })
 
-# kraken_server_time, kraken_server_status,
+test_that('the "kraken_server_time" function returns status as a character and
+          timestamp as a character', {
+  expect_type(kraken_server_status()$status, 'character')
+  expect_type(kraken_server_status()$timestamp, 'character')
+})
+
+#' all_asset_info <- kraken_asset_info()
+#' eth_btc_info <- kraken_asset_info("ETH,BTC")
+#' currency_info <- kraken_asset_info(aclass = "currency")
+
 # kraken_asset_info, kraken_asset_pairs,
 # kraken_ticker_info, kucoin_time, kucoin_symbols_list,
 # magic_eden_collection_stats,
