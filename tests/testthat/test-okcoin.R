8244cfbbd42a2e4d7b7fe97bb24b2abbc8ac4f0c
Trevor French <trevorf96@gmail.com>
2023-12-10 10:05:31 -0500

    okcoin unit tests

--- tests/testthat/test-okcoin.R
+++ tests/testthat/test-okcoin.R
@@ -6,9 +6,6 @@ test_that('the "okcocin_trading_pairs" function returns a dataframe', {
   expect_s3_class(okcoin_trading_pairs(), 'data.frame')
 })
 
-
-
-
-# magic_eden_token_listings, magic_eden_token_metadata,
-# okcoin_trading_pairs, okcoin_time
-# bittrex source, c source xt
+test_that('the "okcocin_time" function returns a character', {
+  expect_type(okcoin_time(), 'character')
+})
