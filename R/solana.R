e4258339ab326d6e20b78e7764d53e1ce7d01baf
Trevor French <trevorf96@gmail.com>
2023-12-12 21:02:58 -0500

    solana_ prefixes

--- R/solana.R
+++ R/solana.R
@@ -13,17 +13,19 @@
 
 solana_api_call <- function(url, request_body) {
   headers <- c("Content-Type" = "application/json")
-  response <- httr::POST(url, httr::add_headers(.headers = headers), body = request_body)
+  response <-
+    httr::POST(url, httr::add_headers(.headers = headers), body = request_body)
 
   if (httr::status_code(response) == 200) {
     return(httr::content(response))
   } else {
     print(httr::content(response, as = "text"))
-    stop("Request failed with status code: ", httr::status_code(response))
+    stop("Request failed with status code: ",
+         httr::status_code(response))
   }
 }
 
-#' assemble_key_pair
+#' solana_assemble_key_pair
 #'
 #' @param key the key for your key pair
 #' @param pair the pair for your key pair
@@ -37,16 +39,16 @@ solana_api_call <- function(url, request_body) {
 #' limit <- NULL
 #' limit <- assemble_key_pair('limit', limit)}
 
-assemble_key_pair <- function(key, pair) {
-  if(is.null(pair)){
-    keypair = ''
+solana_assemble_key_pair <- function(key, pair) {
+  if (is.null(pair)) {
+    keypair <- ''
   } else {
-    keypair = paste('"',key,'":',pair, sep = '')
+    keypair <- paste('"', key, '":', pair, sep = '')
   }
   return(keypair)
 }
 
-#' assemble_config_object
+#' solana_assemble_list
 #'
 #' @param character_vector the character vector used to create the config object
 #' @return Returns your config object
@@ -57,13 +59,13 @@ assemble_key_pair <- function(key, pair) {
 #' limit <- NULL
 #' limit <- assemble_key_pair('limit', limit)}
 
-assemble_list <- function(character_vector) {
+solana_assemble_list <- function(character_vector) {
   character_vector <- character_vector[character_vector != '']
   character_vector <- paste(character_vector, collapse = ',')
   return(character_vector)
 }
 
-#' assemble_request_body
+#' solana_assemble_request_body
 #'
 #' @param jsonrpc the jsonrpc for your request body
 #' @param id the id for your request body
@@ -79,18 +81,19 @@ assemble_list <- function(character_vector) {
 #' params <- paste('["',address,'", {',limit,'}]', sep = '')
 #' request_body <- assemble_request_body('2.0', 'null', 'getSignaturesForAddress', params)}
 
-assemble_request_body <- function(jsonrpc, id, method, params) {
-  jsonrpc <- assemble_key_pair('jsonrpc', jsonrpc)
-  id <- assemble_key_pair('id', id)
-  method <- assemble_key_pair('method', method)
-  params <- assemble_key_pair('params', params)
-  character_vector <- c(jsonrpc, id, method, params)
-  body <- assemble_list(character_vector)
-  request_body <- paste('{',body,'}', sep = '')
-  return(request_body)
-}
+solana_assemble_request_body <-
+  function(jsonrpc, id, method, params) {
+    jsonrpc <- assemble_key_pair('jsonrpc', jsonrpc)
+    id <- assemble_key_pair('id', id)
+    method <- assemble_key_pair('method', method)
+    params <- assemble_key_pair('params', params)
+    character_vector <- c(jsonrpc, id, method, params)
+    body <- assemble_list(character_vector)
+    request_body <- paste('{', body, '}', sep = '')
+    return(request_body)
+  }
 
-#' get_signature_for_address
+#' solana_get_signature_for_address
 #'
 #' @param url the RPC url for your API call
 #' @param address the address for which you're retrieving signatures
@@ -107,16 +110,18 @@ assemble_request_body <- function(jsonrpc, id, method, params) {
 #' url <- "https://api.devnet.solana.com"
 #' data <- get_signature_for_address(url)}
 
-get_signature_for_address <- function(url, address, limit = NULL) {
-  limit <- assemble_key_pair('limit', limit)
-  character_vector <- c(limit)
-  config_object <- assemble_list(character_vector)
-  params <- paste('["',address,'", {',config_object,'}]', sep = '')
-  request_body <- assemble_request_body('"2.0"', 'null', '"getSignaturesForAddress"', params)
-  solana_api_call(url, request_body)
-}
+solana_get_signature_for_address <-
+  function(url, address, limit = NULL) {
+    limit <- assemble_key_pair('limit', limit)
+    character_vector <- c(limit)
+    config_object <- assemble_list(character_vector)
+    params <- paste('["', address, '", {', config_object, '}]', sep = '')
+    request_body <-
+      assemble_request_body('"2.0"', 'null', '"getSignaturesForAddress"', params)
+    solana_api_call(url, request_body)
+  }
 
-#' get_account_info
+#' solana_get_account_info
 #'
 #' @param url the RPC url for your API call
 #' @param address the address for which you're retrieving account info
@@ -130,13 +135,14 @@ get_signature_for_address <- function(url, address, limit = NULL) {
 #' address <- "72tXz6jhGVPFE8ZfAQocJPJU3HgxsdrRqKZoUdWUhs7o"
 #' data <- get_account_info(url, address)}
 
-get_account_info <- function(url, address) {
-  params <- paste('["',address,'"]', sep = '')
-  request_body <- assemble_request_body('"2.0"', 'null', '"getAccountInfo"', params)
+solana_get_account_info <- function(url, address) {
+  params <- paste('["', address, '"]', sep = '')
+  request_body <-
+    assemble_request_body('"2.0"', 'null', '"getAccountInfo"', params)
   solana_api_call(url, request_body)
 }
 
-#' get_block_height
+#' solana_get_block_height
 #'
 #' @param url the RPC url for your API call
 #'
@@ -148,12 +154,13 @@ get_account_info <- function(url, address) {
 #' url <- "https://api.devnet.solana.com"
 #' data <- get_block_height(url)}
 
-get_block_height <- function(url, address) {
-  request_body <- assemble_request_body('"2.0"', 'null', '"getBlockHeight"', NULL)
+solana_get_block_height <- function(url, address) {
+  request_body <-
+    assemble_request_body('"2.0"', 'null', '"getBlockHeight"', NULL)
   solana_api_call(url, request_body)
 }
 
-#' get_health
+#' solana_get_health
 #'
 #' @param url the RPC url for your API call
 #'
@@ -165,12 +172,13 @@ get_block_height <- function(url, address) {
 #' url <- "https://api.devnet.solana.com"
 #' data <- get_health(url)}
 
-get_health <- function(url, address) {
-  request_body <- assemble_request_body('"2.0"', 'null', '"getHealth"', NULL)
+solana_get_health <- function(url, address) {
+  request_body <-
+    assemble_request_body('"2.0"', 'null', '"getHealth"', NULL)
   solana_api_call(url, request_body)
 }
 
-#' get_version
+#' solana_get_version
 #'
 #' @param url the RPC url for your API call
 #'
@@ -182,12 +190,13 @@ get_health <- function(url, address) {
 #' url <- "https://api.devnet.solana.com"
 #' data <- get_version(url)}
 
-get_version <- function(url, address) {
-  request_body <- assemble_request_body('"2.0"', 'null', '"getVersion"', NULL)
+solana_get_version <- function(url, address) {
+  request_body <-
+    assemble_request_body('"2.0"', 'null', '"getVersion"', NULL)
   solana_api_call(url, request_body)
 }
 
-#' get_supply
+#' solana_get_supply
 #'
 #' @param url the RPC url for your API call
 #'
@@ -199,12 +208,13 @@ get_version <- function(url, address) {
 #' url <- "https://api.devnet.solana.com"
 #' data <- get_supply(url)}
 
-get_supply <- function(url, address) {
-  request_body <- assemble_request_body('"2.0"', 'null', '"getSupply"', NULL)
+solana_get_supply <- function(url, address) {
+  request_body <-
+    assemble_request_body('"2.0"', 'null', '"getSupply"', NULL)
   solana_api_call(url, request_body)
 }
 
-#' get_identity
+#' solana_get_identity
 #'
 #' @param url the RPC url for your API call
 #'
@@ -216,12 +226,13 @@ get_supply <- function(url, address) {
 #' url <- "https://api.devnet.solana.com"
 #' data <- get_identity(url)}
 
-get_identity <- function(url, address) {
-  request_body <- assemble_request_body('"2.0"', 'null', '"getIdentity"', NULL)
+solana_get_identity <- function(url, address) {
+  request_body <-
+    assemble_request_body('"2.0"', 'null', '"getIdentity"', NULL)
   solana_api_call(url, request_body)
 }
 
-#' get_inflation_rate
+#' solana_get_inflation_rate
 #'
 #' @param url the RPC url for your API call
 #'
@@ -233,12 +244,13 @@ get_identity <- function(url, address) {
 #' url <- "https://api.devnet.solana.com"
 #' data <- get_inflation_rate(url)}
 
-get_inflation_rate <- function(url, address) {
-  request_body <- assemble_request_body('"2.0"', 'null', '"getInflationRate"', NULL)
+solana_get_inflation_rate <- function(url, address) {
+  request_body <-
+    assemble_request_body('"2.0"', 'null', '"getInflationRate"', NULL)
   solana_api_call(url, request_body)
 }
 
-#' get_genesis_hash
+#' solana_get_genesis_hash
 #'
 #' @param url the RPC url for your API call
 #'
@@ -250,7 +262,8 @@ get_inflation_rate <- function(url, address) {
 #' url <- "https://api.devnet.solana.com"
 #' data <- get_genesis_hash(url)}
 
-get_genesis_hash <- function(url, address) {
-  request_body <- assemble_request_body('"2.0"', 'null', '"getGenesisHash"', NULL)
+solana_get_genesis_hash <- function(url, address) {
+  request_body <-
+    assemble_request_body('"2.0"', 'null', '"getGenesisHash"', NULL)
   solana_api_call(url, request_body)
 }
