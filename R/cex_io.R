59147f3aa2ba79f8e07fae18a2b1f90b743dd12c
Trevor French <trevorf96@gmail.com>
2024-06-05 14:46:25 -0400

    cex io graceful exits and timeouts

--- R/cex_io.R
+++ R/cex_io.R
@@ -2,6 +2,7 @@
 #'
 #' @param symbol_1 the first currency in your pair
 #' @param symbol_2 the second currency in your pair
+#' @param timeout_seconds seconds until the query times out. Default is 60.
 #'
 #' @return returns a list with basic trading information about your specified
 #' currency pair for the last 24 hours.
@@ -12,14 +13,29 @@
 #' symbol_2 <- 'usd'
 #' cex_io_ticker(symbol_1, symbol_2)
 
-cex_io_ticker <- function(symbol_1, symbol_2) {
-  res <- httr::GET(paste('https://cex.io/api/ticker/', toupper(symbol_1), '/',
-                         toupper(symbol_2), sep = ''))
-  data <- jsonlite::fromJSON(rawToChar(res$content))
-  return(data)
+cex_io_ticker <- function(symbol_1, symbol_2, timeout_seconds = 60) {
+  tryCatch({
+    res <- httr::GET(paste('https://cex.io/api/ticker/', toupper(symbol_1), '/',
+                           toupper(symbol_2), sep = '')
+                     , httr::timeout(timeout_seconds))
+
+    if (res$status_code == 200) {
+      data <- jsonlite::fromJSON(rawToChar(res$content))
+      return(data)
+    } else {
+      stop(paste("API call failed with status code", res$status_code))
+    }
+
+  }, error = function(e) {
+    message <- paste("Error during API call:", e$message)
+    warning(message)
+    return(NULL)
+  })
 }
 
 #' cex_io_currency_limits
+#'
+#' @param timeout_seconds seconds until the query times out. Default is 60.
 #'
 #' @return returns a dataframe with information about currency limits on CEX.io.
 #' @export
@@ -27,16 +43,30 @@ cex_io_ticker <- function(symbol_1, symbol_2) {
 #' @examples
 #' cex_io_currency_limits()
 
-cex_io_currency_limits <- function() {
-  res <- httr::GET('https://cex.io/api/currency_limits')
-  data <- jsonlite::fromJSON(rawToChar(res$content))
-  return(data$data$pairs)
+cex_io_currency_limits <- function(timeout_seconds = 60) {
+  tryCatch({
+    res <- httr::GET('https://cex.io/api/currency_limits'
+                     , httr::timeout(timeout_seconds))
+
+    if (res$status_code == 200) {
+      data <- jsonlite::fromJSON(rawToChar(res$content))
+      return(data)
+    } else {
+      stop(paste("API call failed with status code", res$status_code))
+    }
+
+  }, error = function(e) {
+    message <- paste("Error during API call:", e$message)
+    warning(message)
+    return(NULL)
+  })
 }
 
 #' cex_io_last_price
 #'
 #' @param symbol_1 the first currency in your pair
 #' @param symbol_2 the second currency in your pair
+#' @param timeout_seconds seconds until the query times out. Default is 60.
 #'
 #' @return returns a list with the last price of your specified currency pair.
 #' @export
@@ -46,11 +76,23 @@ cex_io_currency_limits <- function() {
 #' symbol_2 <- 'usd'
 #' cex_io_last_price(symbol_1, symbol_2)
 
-cex_io_last_price <- function(symbol_1, symbol_2) {
-  res <- httr::GET(paste('https://cex.io/api/last_price/', toupper(symbol_1),
-                         '/', toupper(symbol_2), sep = ''))
-  data <- jsonlite::fromJSON(rawToChar(res$content))
-  return(data)
+cex_io_last_price <- function(symbol_1, symbol_2, timeout_seconds = 60) {
+  tryCatch({
+    res <- httr::GET(paste('https://cex.io/api/last_price/', toupper(symbol_1),
+                           '/', toupper(symbol_2), sep = '')
+                     , httr::timeout(timeout_seconds))
+    if (res$status_code == 200) {
+      data <- jsonlite::fromJSON(rawToChar(res$content))
+      return(data)
+    } else {
+      stop(paste("API call failed with status code", res$status_code))
+    }
+
+  }, error = function(e) {
+    message <- paste("Error during API call:", e$message)
+    warning(message)
+    return(NULL)
+  })
 }
 
 #' cex_io_converter
@@ -58,6 +100,7 @@ cex_io_last_price <- function(symbol_1, symbol_2) {
 #' @param symbol_1 the first currency in your pair
 #' @param symbol_2 the second currency in your pair
 #' @param amount the currency amount to convert denominated in symbol_1
+#' @param timeout_seconds seconds until the query times out. Default is 60.
 #'
 #' @return returns the converted amount denominated in symbol_2
 #' @export
@@ -68,15 +111,34 @@ cex_io_last_price <- function(symbol_1, symbol_2) {
 #' amount <- '2.5'
 #' cex_io_converter(symbol_1, symbol_2, amount)
 
-cex_io_converter <- function(symbol_1, symbol_2, amount) {
-  res <- httr::POST(paste('https://cex.io/api/convert/', toupper(symbol_1), '/',
-                          toupper(symbol_2), sep = '')
-                   , body = paste('{"amnt": "', amount, '"}', sep = '')
-                   , httr::add_headers('Content-Type' = 'application/json'
-                                      , 'Accept' = '*/*'
-                   ))
-  data <- jsonlite::fromJSON(rawToChar(res$content))
-  return(data$amnt)
+cex_io_converter <- function(symbol_1, symbol_2, amount, timeout_seconds = 60) {
+  tryCatch({
+    res <- httr::POST(paste('https://cex.io/api/convert/', toupper(symbol_1), '/',
+                            toupper(symbol_2), sep = '')
+                      , body = paste('{"amnt": "', amount, '"}', sep = '')
+                      , httr::add_headers('Content-Type' = 'application/json'
+                                          , 'Accept' = '*/*'
+                      )
+                      , httr::timeout(timeout_seconds))
+    if (res$status_code == 200) {
+      data <- jsonlite::fromJSON(rawToChar(res$content))
+
+      if (!is.null(data$amnt)) {
+        return(data$amnt)
+      } else {
+        warning("The response does not contain 'payload'.")
+        return(NULL)
+      }
+
+    } else {
+      stop(paste("API call failed with status code", res$status_code))
+    }
+
+  }, error = function(e) {
+    message <- paste("Error during API call:", e$message)
+    warning(message)
+    return(NULL)
+  })
 }
 
 #' cex_io_ohlcv
@@ -84,6 +146,7 @@ cex_io_converter <- function(symbol_1, symbol_2, amount) {
 #' @param date the date for which to retrieve data
 #' @param symbol_1 the first currency in your pair
 #' @param symbol_2 the second currency in your pair
+#' @param timeout_seconds seconds until the query times out. Default is 60.
 #'
 #' @return returns a list containing open, high, low, close, volume data for the
 #' past minute, hour, and day
@@ -95,12 +158,24 @@ cex_io_converter <- function(symbol_1, symbol_2, amount) {
 #' symbol_2 <- 'usd'
 #' cex_io_ohlcv(date, symbol_1, symbol_2)
 
-cex_io_ohlcv <- function(date, symbol_1, symbol_2) {
-  res <- httr::GET(paste('https://cex.io/api/ohlcv/hd/',
-                         date, '/', toupper(symbol_1), '/',
-                         toupper(symbol_2), sep = ''))
-  data <- jsonlite::fromJSON(rawToChar(res$content))
-  return(data)
+cex_io_ohlcv <- function(date, symbol_1, symbol_2, timeout_seconds = 60) {
+  tryCatch({
+    res <- httr::GET(paste('https://cex.io/api/ohlcv/hd/',
+                           date, '/', toupper(symbol_1), '/',
+                           toupper(symbol_2), sep = '')
+                     , httr::timeout(timeout_seconds))
+    if (res$status_code == 200) {
+      data <- jsonlite::fromJSON(rawToChar(res$content))
+      return(data)
+    } else {
+      stop(paste("API call failed with status code", res$status_code))
+    }
+
+  }, error = function(e) {
+    message <- paste("Error during API call:", e$message)
+    warning(message)
+    return(NULL)
+  })
 }
 
 #' cex_io_nonce
@@ -151,6 +226,7 @@ cex_io_signature <- function(username, api_key, api_secret, nonce) {
 #' @param username your cex.io username
 #' @param api_key your cex.io api_key
 #' @param api_secret your cex.io api_secret
+#' @param timeout_seconds seconds until the query times out. Default is 60.
 #'
 #' @return returns a list with your balances for each currency
 #' @export
@@ -162,17 +238,31 @@ cex_io_signature <- function(username, api_key, api_secret, nonce) {
 #' api_secret <- "..."
 #' balances <- cex_io_balance(username, api_key, api_secret)}
 
-cex_io_balance <- function(username, api_key, api_secret) {
+cex_io_balance <- function(username, api_key, api_secret, timeout_seconds = 60) {
   nonce <- cex_io_nonce()
   sig <- cex_io_signature(username, api_key, api_secret, nonce)
   request_body <- paste('{"key": "', api_key
                         , '", "signature": "', sig
                         , '", "nonce": "', nonce, '"}'
                         , sep = '')
-  res <- httr::POST('https://cex.io/api/balance/'
-                   , httr::add_headers('Content-Type' = 'application/json'
-                                       , 'Accept' = '*/*')
-                   , body = request_body)
-  data <- jsonlite::fromJSON(rawToChar(res$content))
-  return(data)
+
+  tryCatch({
+    res <- httr::POST('https://cex.io/api/balance/'
+                      , httr::add_headers('Content-Type' = 'application/json'
+                                          , 'Accept' = '*/*')
+                      , httr::timeout(timeout_seconds)
+                      , body = request_body)
+    if (res$status_code == 200) {
+      data <- jsonlite::fromJSON(rawToChar(res$content))
+      return(data)
+    } else {
+      stop(paste("API call failed with status code", res$status_code))
+    }
+
+  }, error = function(e) {
+    message <- paste("Error during API call:", e$message)
+    warning(message)
+    return(NULL)
+  })
 }
+
