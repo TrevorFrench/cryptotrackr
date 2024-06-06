87fb07361c937073d69a10bfbd65a9e08e49c2b8
Trevor French <trevorf96@gmail.com>
2024-06-06 09:28:02 -0400

    paxos graceful exits

--- R/paxos.R
+++ R/paxos.R
@@ -21,11 +21,31 @@ paxos_bearer_token <- function(client_id, client_secret, scope, timeout_seconds 
                       , 'client_id' = client_id
                       , 'client_secret' = client_secret
                       , 'scope' = scope)
-  res <- httr::POST('https://oauth.paxos.com/oauth2/token'
-                    , body = request_body
-                    , httr::timeout(timeout_seconds))
-  data <- jsonlite::fromJSON(rawToChar(res$content))
-  return(data$access_token)
+
+  tryCatch({
+    res <- httr::POST('https://oauth.paxos.com/oauth2/token'
+                      , body = request_body
+                      , httr::timeout(timeout_seconds))
+
+    if (res$status_code == 200) {
+      data <- jsonlite::fromJSON(rawToChar(res$content))
+
+      if (!is.null(data$access_token)) {
+        return(data$access_token)
+      } else {
+        warning("The response does not contain 'access_token'.")
+        return(NULL)
+      }
+
+    } else {
+      stop(paste("API call failed with status code", res$status_code))
+    }
+
+  }, error = function(e) {
+    message <- paste("Error during API call:", e$message)
+    warning(message)
+    return(NULL)
+  })
 }
 
 #' paxos_list_profiles
@@ -46,12 +66,31 @@ paxos_bearer_token <- function(client_id, client_secret, scope, timeout_seconds 
 
 paxos_list_profiles <- function(client_id, client_secret, timeout_seconds = 60) {
   token <- paxos_bearer_token(client_id, client_secret, 'funding:read_profile')
-  res <- httr::GET('https://api.paxos.com/v2/profiles',
-                  httr::add_headers('Content-Type' = 'application/json'
-                                    , 'accept' = 'application/json'
-                                    , 'Authorization' = paste('Bearer ', token, sep = '')
-                  )
-                  , httr::timeout(timeout_seconds))
-  data <- jsonlite::fromJSON(rawToChar(res$content))
-  return(data$items)
+  tryCatch({
+    res <- httr::GET('https://api.paxos.com/v2/profiles',
+                     httr::add_headers('Content-Type' = 'application/json'
+                                       , 'accept' = 'application/json'
+                                       , 'Authorization' = paste('Bearer ', token, sep = '')
+                     )
+                     , httr::timeout(timeout_seconds))
+
+    if (res$status_code == 200) {
+      data <- jsonlite::fromJSON(rawToChar(res$content))
+
+      if (!is.null(data$items)) {
+        return(data$items)
+      } else {
+        warning("The response does not contain 'items'.")
+        return(NULL)
+      }
+
+    } else {
+      stop(paste("API call failed with status code", res$status_code))
+    }
+
+  }, error = function(e) {
+    message <- paste("Error during API call:", e$message)
+    warning(message)
+    return(NULL)
+  })
 }
